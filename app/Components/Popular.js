66909ece9a0762420daf5ba3b475e9da053492fe
= <=>
2019-12-21 11:11:58 +1300

    Final Features

--- app/Components/Popular.js
+++ app/Components/Popular.js
@@ -1,91 +1,142 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { fetchPopularRepos } from '../utils/api';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { fetchPopularRepos } from '../utils/api'
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
+import Loading from './Loading'
+import Tooltip from './Tooltip'
 
-function LanguagesNav({selectedLanguage, updateLanguage}) {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python', 'PHP'];
-    return (
-        <ul className='flex-center'>
-            {languages.map((language) => (
-                <li key={language}>
-                    <button 
-                        style={ language === selectedLanguage ? {color:'rgb(187, 46, 31)'} : null} 
-                        className='btn-clear nav-link'
-                        onClick={() => updateLanguage(language)}
-                        >
-                        {language}
-                    </button>
-                </li>
-            ))}
-        </ul>
-    );
+function LangaugesNav ({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
+  return (
+    <ul className='flex-center'>
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className='btn-clear nav-link'
+            style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+            onClick={() => onUpdateLanguage(language)}>
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
-LanguagesNav.propTypes = {
-    selectedLanguage: PropTypes.string.isRequired,
-    updateLanguage: PropTypes.func.isRequired
-};
+LangaugesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+}
 
-class Popular extends React.Component {
+function ReposGrid ({ repos }) {
+  return (
+    <ul className='grid space-around'>
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
+        const { login, avatar_url } = owner
 
-    constructor(props){
-        super(props);
-        this.state = {
-            selectedLanguage: 'All',
-            error: null,
-            repos: {}
-        };
-
-        this.updateLanguage = this.updateLanguage.bind(this);
-        this.isLoading = this.isLoading.bind(this);
-    }
-
-    componentDidMount(){
-        this.updateLanguage(this.state.selectedLanguage);
-    }
-
-    updateLanguage(selectedLanguage){
-        this.setState({
-            selectedLanguage: selectedLanguage,
-            error: null
-        });        
-        if ( !this.state.repos[selectedLanguage] ) {
-            fetchPopularRepos(selectedLanguage)
-                .then((data) => {
-                    this.setState(({repos}) => ({
-                        repos:{
-                            ...repos,
-                            [selectedLanguage]: data
-                        }
-                    }))
-                })
-                .catch(() => {
-                    console.warn('Error fetching repos: ', error);
-                    this.setState({
-                        error: `There was an error fetching the repositories.`
-                    });
-                });
-        }
-        
-    }
-
-    isLoading(){
-        return this.state.error === null && !this.state.repos[this.state.selectedLanguage];
-    }
-    
-    render(){
-        const {selectedLanguage, repos, error} = this.state;
         return (
-        <React.Fragment>
-        <LanguagesNav 
-            selectedLanguage={selectedLanguage}
-            updateLanguage={this.updateLanguage}    
-        />
-        {this.isLoading() && <p>LOADING..</p>}
-        {error && <p>{error}</p>}
-        {repos[selectedLanguage] && <pre>{JSON.stringify(repos, null, 2)}</pre>}
-        </React.Fragment>);
-    }
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className='card-list'>
+                <li>
+                  <Tooltip text="Github Username">
+                  <FaUser color='rgb(255, 191, 116)' size={22} />
+                  <a href={`https://github.com/${login}`}>
+                    {login}
+                  </a>
+                  </Tooltip>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </Card>
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
-export default Popular;
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+}
+
+export default class Popular extends React.Component {
+
+  state = {
+    selectedLanguage: 'All',
+    repos: {},
+    error: null,
+  }
+
+  componentDidMount = () => {
+    this.updateLanguage(this.state.selectedLanguage)
+  }
+
+  updateLanguage = (selectedLanguage) => {
+    this.setState({
+      selectedLanguage,
+      error: null,
+    })
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
+          }))
+        })
+        .catch(() => {
+          console.warn('Error fetching repos: ', error)
+
+          this.setState({
+            error: `There was an error fetching the repositories.`
+          })
+        })
+    }
+  }
+  isLoading = () => {
+    const { selectedLanguage, repos, error } = this.state
+
+    return !repos[selectedLanguage] && error === null
+  }
+  render() {
+    const { selectedLanguage, repos, error } = this.state
+
+    return (
+      <React.Fragment>
+        <LangaugesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+
+        {this.isLoading() && <Loading text='Fetching Repos' />}
+
+        {error && <p className='center-text error'>{error}</p>}
+
+        {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
+      </React.Fragment>
+    )
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Popular from './Components/Popular';
+
+import { ThemeProvider } from './contexts/theme';
+import Nav from './Components/Nav';
+import Loading from './Components/Loading';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+const Popular = React.lazy(() => import('./Components/Popular'));
+const Battle = React.lazy(() => import('./Components/Battle'));
+const Results = React.lazy(() => import('./Components/Results'));
+
+class App extends React.Component {
+
+    state = {
+        theme: 'light',
+        toggleTheme: () => {
+            this.setState(({ theme }) => ({
+                theme: theme === 'light' ? 'dark' : 'light'
+            }))
+        }
+    };
+    
+    render() {
+        return (
+            <Router>
+                <ThemeProvider value={this.state}>
+                    <div className={this.state.theme}>
+                        <div className='container'>
+                            <Nav />
+                            <React.Suspense fallback={<Loading/>}>
+                                <Switch>
+                                    <Route exact path="/" component={Popular} />
+                                    <Route exact path="/battle" component={Battle} />
+                                    <Route path="/battle/results" component={Results} />
+                                    <Route render={() => <h1>404</h1>} />
+                                </Switch>
+                            </React.Suspense>
+                        </div>
+                    </div>
+                </ThemeProvider>
+            </Router>
+        );
+    }
+}
 
 
-ReactDOM.render(<Popular/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
